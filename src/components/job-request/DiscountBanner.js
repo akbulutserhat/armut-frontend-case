1094a7571f031b67c80620b5a086f40d56b0a7ed
akbulutserhat <serhatakbulut03@gmail.com>
2021-03-13 03:44:59 +0300

    icons has been added

--- src/components/job-request/DiscountBanner.js
+++ src/components/job-request/DiscountBanner.js
@@ -1,4 +1,17 @@
 const DiscountBanner = ({ discountRateText }) => {
+  const percentNumberPattern = /%\d+/;
+  const percentNumber = discountRateText.match(percentNumberPattern)[0];
+  const emphasizedPercentNumber = (
+    <span className='emphasized-text'>{percentNumber}</span>
+  );
+  const withoutPercentNumber = discountRateText.split(percentNumber);
+  const modifiedText = [
+    withoutPercentNumber[0],
+    emphasizedPercentNumber,
+    withoutPercentNumber[1],
+  ].join('');
+
+  console.log(modifiedText);
   return (
     <div className='discount-banner'>
       <p className='medium lh-22'>{discountRateText}</p>
--- src/components/job-request/Header.js
+++ src/components/job-request/Header.js
@@ -1,7 +1,16 @@
+import closeIcon from '../../assets/Close.png';
+import prevIcon from '../../assets/Left.png';
+
 const Header = ({ name }) => {
   return (
     <div className='header'>
       <p className='small lh-18'>{name}</p>
+      <div className='close-icon'>
+        <img src={closeIcon}></img>
+      </div>
+      <div className='prev-icon'>
+        <img src={prevIcon}></img>
+      </div>
     </div>
   );
 };
